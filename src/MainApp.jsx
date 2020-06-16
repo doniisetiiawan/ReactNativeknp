ade7eac6f4efd994787ca267234558f479064b72
Doni Setiawan <donisetiawan.050@gmail.com>
2020-06-16 13:40:08 +0700

    Using flexbox to create a profile page

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,133 +1,137 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Image,
   StyleSheet,
-  Text,
   View,
+  Image,
+  Text,
 } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import homeIcon from './images/home.png';
-import favIcon from './images/star.png';
-import blogIcon from './images/notebook.png';
-import profileIcon from './images/user.png';
+import profileImage from './images/user-profile.jpg';
+import friendsIcon from './images/profile.png';
+import favIcon from './images/plain-heart.png';
+import msgIcon from './images/chat.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: null,
+    width: null,
   },
-  title: {
-    fontSize: 20,
-    marginTop: 20,
+  info: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    top: null,
+  },
+  personal: {
+    padding: 30,
+  },
+  name: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  occupation: {
+    color: '#d6ec1b',
+    marginTop: 5,
+  },
+  stats: {
+    flexDirection: 'row',
+  },
+  stat: {
+    alignItems: 'center',
+    backgroundColor: '#7675b7',
+    borderColor: '#6e6db1',
+    borderLeftWidth: 1,
+    flex: 1,
+    padding: 10,
+  },
+  selected: {
+    tintColor: '#d6ec1b',
   },
   icon: {
-    width: 30,
+    tintColor: '#504f9f',
     height: 30,
-    tintColor: '#42b49a',
+    width: 30,
+  },
+  counter: {
+    color: '#fff',
+    fontSize: 15,
+    marginTop: 5,
   },
 });
 
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Image source={homeIcon} style={styles.icon} />
-      <Text style={styles.title}>Home</Text>
+class MainApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'Crysfel',
+      lastName: 'Villa Roman',
+      occupation: 'Software Engineer',
+      friends: '1,200',
+      favorites: '2,491',
+      comments: '4,832',
+    };
+  }
+
+  renderStat = (options) => (
+    <View style={styles.stat}>
+      <Image
+        source={options.icon}
+        style={[
+          styles.icon,
+          options.selected ? styles.selected : null,
+        ]}
+      />
+      <Text style={styles.counter}>{options.value}</Text>
     </View>
   );
-}
 
-function FavoritesScreen() {
-  return (
-    <View style={styles.container}>
-      <Image source={favIcon} style={styles.icon} />
-      <Text style={styles.title}>Favorites</Text>
-    </View>
-  );
-}
+  render() {
+    const {
+      name,
+      lastName,
+      occupation,
+      friends,
+      favorites,
+      comments,
+    } = this.state;
 
-function BlogScreen() {
-  return (
-    <View style={styles.container}>
-      <Image source={blogIcon} style={styles.icon} />
-      <Text style={styles.title}>Blog</Text>
-    </View>
-  );
-}
-
-function ProfileScreen() {
-  return (
-    <View style={styles.container}>
-      <Image source={profileIcon} style={styles.icon} />
-      <Text style={styles.title}>Profile</Text>
-    </View>
-  );
-}
-
-function TabBarIcon(props) {
-  return <Image source={props.icon} style={styles.icon} />;
-}
-
-const Tab = createBottomTabNavigator();
-
-function MainApp() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <TabBarIcon
-                focused={focused}
-                icon={homeIcon}
-              />
-            ),
-          }}
+    return (
+      <>
+        <Image
+          source={profileImage}
+          style={styles.container}
         />
-        <Tab.Screen
-          name="Favorites"
-          component={FavoritesScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <TabBarIcon
-                focused={focused}
-                icon={favIcon}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Blog"
-          component={BlogScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <TabBarIcon
-                focused={focused}
-                icon={blogIcon}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <TabBarIcon
-                focused={focused}
-                icon={profileIcon}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+
+        <View style={styles.info}>
+          <View style={styles.personal}>
+            <Text style={styles.name}>
+              {name} {lastName}
+            </Text>
+            <Text style={styles.occupation}>
+              {occupation.toUpperCase()}
+            </Text>
+          </View>
+          <View style={styles.stats}>
+            {this.renderStat({
+              icon: friendsIcon,
+              value: friends,
+              selected: true,
+            })}
+            {this.renderStat({
+              icon: favIcon,
+              value: favorites,
+            })}
+            {this.renderStat({
+              icon: msgIcon,
+              value: comments,
+            })}
+          </View>
+        </View>
+      </>
+    );
+  }
 }
 
 export default MainApp;
