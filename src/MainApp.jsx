720cccc957b5602ca0dd23c4d5ee24b3b2785b5b
Doni Setiawan <donisetiawan.050@gmail.com>
2020-06-18 12:56:13 +0700

    Running several animations at the same time

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 import {
+  View,
   Animated,
+  Image,
   Easing,
   Dimensions,
   StyleSheet,
 } from 'react-native';
-import cloudImage from './images/cloudy.png';
+import cloudImage from './images/cloud.png';
+import cloudsImage from './images/cloudy.png';
+import planeImage from './images/transport.png';
 
 const { width, height } = Dimensions.get('window');
-
-const imageWidth = 80;
+const cloudWidth = 60;
 
 const styles = StyleSheet.create({
-  image: {
-    height: imageWidth,
+  cloud1: {
     position: 'absolute',
-    top: height / 3,
-    width: imageWidth,
+    width: cloudWidth,
+    height: cloudWidth,
+    top: height / 3 - cloudWidth / 2,
+  },
+  cloud2: {
+    width: cloudWidth * 1.5,
+    height: cloudWidth * 1.5,
+    top: height / 2,
+  },
+  plane: {
+    width: cloudWidth * 1.3,
+    height: cloudWidth * 1.3,
+    top: height / 2 - cloudWidth,
+    left: width / 2 - cloudWidth / 2,
   },
 });
 
@@ -31,20 +45,47 @@ class MainApp extends Component {
   };
 
   startAnimation = () => {
-    this.animatedValue.setValue(width);
+    this.animatedValue.setValue(1);
     Animated.timing(this.animatedValue, {
-      toValue: -imageWidth,
+      toValue: 0,
       duration: 6000,
       easing: Easing.linear,
     }).start(() => this.startAnimation());
   };
 
   render() {
+    const left1 = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-cloudWidth, width],
+    });
+
+    const left2 = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [
+        -cloudWidth * 5,
+        width + cloudWidth * 5,
+      ],
+    });
+
     return (
-      <Animated.Image
-        style={[styles.image, { left: this.animatedValue }]}
-        source={cloudImage}
-      />
+      <View>
+        <Animated.Image
+          style={[styles.cloud1, { left: left1 }]}
+          source={cloudImage}
+        />
+        <Animated.Image
+          style={[
+            styles.cloud1,
+            styles.cloud2,
+            { left: left2 },
+          ]}
+          source={cloudsImage}
+        />
+        <Image
+          style={[styles.cloud1, styles.plane]}
+          source={planeImage}
+        />
+      </View>
     );
   }
 }
